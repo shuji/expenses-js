b79b6669e30ef955a7a7bbc52f003e29b2a508af
Shuji Watanabe <shuji.w6e@gmail.com>
2013-02-23 15:33:41 +0900

    add enterkey event handler

--- src/view.js
+++ src/view.js
@@ -3,5 +3,13 @@ $(function() {
     console.log('init');
     var model = new EamingAndExpense();
     console.log(model);
+    $('#commandline').bind('keydown', function(e){
+       var code = (e.keyCode ? e.keyCode : e.which);
+       if (code == 13) onEnterKey();
+    });
 });
+
+function onEnterKey() {
+    console.log("#onEnterKey");
+}
 
